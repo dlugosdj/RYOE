29f2d4f7b6bbd9786ba1bee463b911b361ca378a
Daniel Dlugos <dlugosdj@gmail.com>
2025-11-13 18:05:28 +0000

    5th commit

--- queries.sql
+++ queries.sql
@@ -33,3 +33,31 @@ where rush_attempt = 1
 group by rusher_player_id
 having count(*) >= 100
 order by "RYOE" desc;
+
+------------------------------------------------------------------------------------------
+
+select rusher_player_id as "Player Id",
+rusher_player_name as "Player Name",
+yards_gained as "Yards Gained",
+ydstogo as "Yards to Go",
+down as "Down"
+from plays
+where rush_attempt = 1
+
+select rusher_player_id as "Player Id",
+rusher_player_name as "Player Name",
+yards_gained as "Yards Gained",
+ydstogo as "Yards to Go",
+case when down = 2 then 1
+    else 0
+    end as "2nd Down",
+case
+    when down = 3 then 1
+    else 0
+    end as "3rd Down",
+case
+    when down = 4 then 1
+    else 0
+    end as "4th Down"
+from plays
+where rush_attempt = 1;
